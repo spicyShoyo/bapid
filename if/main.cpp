315d9a9d1dadcda5bbc7489a48e9b1d0368ae097
spicyShoyo <zliu263@gmail.com>
2022-09-29 21:23:59 -0700

    grpc

--- if/main.cpp
+++ if/main.cpp
@@ -1,6 +1,47 @@
 #include "if/bapid.grpc.pb.h"
 #include <grpc/grpc.h>
-#include <grpcpp/server.h>
-#include <grpcpp/server_builder.h>
+#include <grpcpp/ext/proto_server_reflection_plugin.h>
+#include <grpcpp/grpcpp.h>
+#include <grpcpp/health_check_service_interface.h>
+#include <string>
 
-int main() { grpc::ServerBuilder{}; }
+namespace bapid {
+using grpc::Server;
+using grpc::ServerBuilder;
+using grpc::ServerContext;
+using grpc::Status;
+
+class GreeterServiceImpl final : public Greeter::Service {
+  Status Ping(ServerContext *context, const PingRequest *request,
+              PingReply *reply) override {
+    reply->set_message("hi " + request->name());
+    return Status::OK;
+  }
+};
+
+void runServer() {
+  std::string server_address("0.0.0.0:50051");
+  GreeterServiceImpl service;
+
+  grpc::EnableDefaultHealthCheckService(true);
+  grpc::reflection::InitProtoReflectionServerBuilderPlugin();
+  ServerBuilder builder;
+  // Listen on the given address without any authentication mechanism.
+  builder.AddListeningPort(server_address, grpc::InsecureServerCredentials());
+  // Register "service" as the instance through which we'll communicate with
+  // clients. In this case it corresponds to an *synchronous* service.
+  builder.RegisterService(&service);
+  // Finally assemble the server.
+  std::unique_ptr<Server> server(builder.BuildAndStart());
+  std::cout << "Server listening on " << server_address << std::endl;
+
+  // Wait for the server to shutdown. Note that some other thread must be
+  // responsible for shutting down the server for this call to ever return.
+  server->Wait();
+}
+} // namespace bapid
+
+int main() {
+  bapid::runServer();
+  return 0;
+}
