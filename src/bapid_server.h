bf2327690bdf9f31aa83fe96d974dc21aa8279be
spicyShoyo <zliu263@gmail.com>
2022-10-01 23:09:34 -0700

    template

--- src/bapid_server.h
+++ src/bapid_server.h
@@ -5,13 +5,34 @@
 #include <grpcpp/completion_queue.h>
 #include <grpcpp/grpcpp.h>
 #include <memory>
+#include <tuple>
+#include <type_traits>
 
 namespace bapid {
-template <typename THandler, typename TRequest, typename TReply,
-          auto TRegisterFn>
-class CallDataBase {
+namespace {
+template <typename TFn> struct function_traits;
+
+template <typename TRes, typename TKlass, typename... TArgs>
+struct function_traits<TRes (TKlass::*)(TArgs...)> {
+  template <size_t i> struct arg {
+    using type = typename std::tuple_element<i, std::tuple<TArgs...>>::type;
+  };
+};
+
+template <typename TWrapper> struct unwrap;
+template <template <typename> class TOuter, typename TInner>
+struct unwrap<TOuter<TInner>> {
+  using type = TInner;
+};
+} // namespace
+
+template <typename THandler, auto TRegisterFn> class CallDataBase {
 public:
-  using type = CallDataBase<THandler, TRequest, TReply, TRegisterFn>;
+  using Request = std::remove_pointer_t<
+      typename function_traits<decltype(TRegisterFn)>::template arg<1>::type>;
+  using Reply = typename unwrap<std::remove_pointer_t<typename function_traits<
+      decltype(TRegisterFn)>::template arg<2>::type>>::type;
+  using type = CallDataBase<THandler, TRegisterFn>;
 
   CallDataBase(BapidService::AsyncService *service,
                grpc::ServerCompletionQueue *cq)
@@ -22,7 +43,7 @@ public:
 
   void proceed() {
     if (!processed_) {
-      new CallDataBase<THandler, TRequest, TReply, TRegisterFn>(service_, cq_);
+      new CallDataBase<THandler, TRegisterFn>(service_, cq_);
       static_cast<THandler *>(this)->process();
       processed_ = true;
       responder_.Finish(reply_, grpc::Status::OK,
@@ -38,15 +59,15 @@ private:
   grpc::ServerCompletionQueue *cq_;
   grpc::ServerContext ctx_;
 
-  TRequest request_;
-  TReply reply_;
+  Request request_;
+  Reply reply_;
 
   grpc::ServerAsyncResponseWriter<PingReply> responder_;
   bool processed_{false};
 };
 
 class PingHandler
-    : public CallDataBase<PingHandler, PingRequest, PingReply,
+    : public CallDataBase<PingHandler,
                           &BapidService::AsyncService::RequestPing> {
 public:
   void process();
