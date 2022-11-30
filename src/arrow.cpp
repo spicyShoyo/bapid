9c33e68be605291755db1a8a42db7a2b4b856191
spicyShoyo <zliu263@gmail.com>
2022-11-29 22:03:08 -0800

    add class representing table

--- src/arrow.cpp
+++ src/arrow.cpp
@@ -1,12 +1,15 @@
+#include "src/arrow.h"
 #include <arrow/api.h>
 #include <arrow/compute/exec/exec_plan.h>
 #include <arrow/dataset/file_parquet.h>
 #include <arrow/filesystem/filesystem.h>
+#include <folly/Expected.h>
 #include <folly/logging/xlog.h>
 #include <iostream>
 #include <memory>
 #include <parquet/arrow/writer.h>
 #include <string_view>
+#include <utility>
 
 namespace bapid {
 
@@ -20,7 +23,7 @@ namespace pq = parquet;
 namespace cp = arrow::compute;
 
 arrow::Result<std::shared_ptr<ds::Dataset>>
-get_dataset(const std::string &root_path) {
+get_fs_dataset(const std::string &root_path) {
   ARROW_ASSIGN_OR_RAISE(auto file_sys, fs::FileSystemFromUriOrPath(root_path));
   auto format = std::make_shared<ds::ParquetFileFormat>();
 
@@ -34,73 +37,157 @@ get_dataset(const std::string &root_path) {
 
   ARROW_ASSIGN_OR_RAISE(auto fragments, dataset->GetFragments())
   for (const auto &fragment : fragments) {
-    std::cout << "Found fragment: " << (*fragment)->ToString() << std::endl;
+    XLOG(INFO) << "Found fragment: " << (*fragment)->ToString() << std::endl;
   }
 
   return dataset;
 }
+} // namespace
 
-arrow::Status
-read_sink(std::shared_ptr<cp::ExecPlan> &&plan,
-          std::shared_ptr<arrow::Schema> &&schema,
-          arrow::AsyncGenerator<std::optional<cp::ExecBatch>> &&sink_gen) {
-  std::shared_ptr<arrow::RecordBatchReader> sink_reader =
-      cp::MakeGeneratorReader(schema, std::move(sink_gen),
+/*static*/
+folly::Expected<std::unique_ptr<BapidTable>, std::string>
+BapidTable::fromFsDataset(const std::string &dataset_dir, std::string name) {
+  auto dataset = get_fs_dataset(dataset_dir);
+  if (!dataset.ok()) {
+    return folly::makeUnexpected(dataset.status().ToString());
+  }
+
+  return std::make_unique<BapidTable>(std::move(name),
+                                      dataset.MoveValueUnsafe());
+}
+
+BapidTable::BapidTable(std::string name, std::shared_ptr<ds::Dataset> dataset)
+    : name_{std::move(name)}, dataset_{std::move(dataset)} {}
+
+SamplesQuery BapidTable::newSamplesQueryX() {
+  return SamplesQuery::fromDataset(dataset_).value();
+}
+
+namespace {
+arrow::Result<SamplesQuery>
+samplesQueryfromDatasetImpl(std::shared_ptr<ds::Dataset> dataset) {
+  auto *registry = cp::default_exec_factory_registry();
+  ds::internal::InitializeScanner(registry);
+  ARROW_ASSIGN_OR_RAISE(auto plan,
+                        cp::ExecPlan::Make(cp::default_exec_context()));
+
+  return SamplesQuery{registry, std::move(plan), std::move(dataset)};
+}
+} // namespace
+
+/*static*/ folly::Expected<SamplesQuery, std::string>
+SamplesQuery::fromDataset(std::shared_ptr<ds::Dataset> dataset) {
+  auto samples_query = samplesQueryfromDatasetImpl(std::move(dataset));
+  if (!samples_query.ok()) {
+    return folly::makeUnexpected(samples_query.status().ToString());
+  }
+
+  return samples_query.MoveValueUnsafe();
+}
+
+SamplesQuery::SamplesQuery(cp::ExecFactoryRegistry *registry,
+                           std::shared_ptr<cp::ExecPlan> plan,
+                           std::shared_ptr<ds::Dataset> dataset)
+    : registry_(registry), plan_{std::move(plan)}, dataset_{
+                                                       std::move(dataset)} {}
+
+SamplesQuery &SamplesQuery::filter() {
+  filters_.emplace_back(
+      cp::greater(cp::field_ref("total_amount"), cp::literal(30)));
+  return *this;
+}
+
+SamplesQuery &SamplesQuery::project() {
+  projects_.emplace_back(cp::field_ref("total_amount"));
+  result_set_schema_.emplace_back(
+      arrow::field("total_amount", arrow::float64()));
+  return *this;
+}
+
+folly::Expected<SamplesQuery::RunnableQuery, std::string>
+SamplesQuery::finalize() && {
+  // TODO(liuz): projection for scanner and validation
+  auto options = std::make_shared<ds::ScanOptions>();
+  options->projection = cp::project({}, {});
+  arrow::AsyncGenerator<std::optional<cp::ExecBatch>> sink_gen;
+
+  decls_.emplace_back(cp::Declaration{"scan", ds::ScanNodeOptions{
+                                                  dataset_,
+                                                  options,
+                                              }});
+  for (auto &filter : filters_) {
+    decls_.emplace_back(
+        cp::Declaration{"filter", cp::FilterNodeOptions{filter}});
+  }
+
+  decls_.emplace_back(
+      cp::Declaration{"project", cp::ProjectNodeOptions{projects_}});
+  decls_.emplace_back(cp::Declaration{"sink", cp::SinkNodeOptions{&sink_gen}});
+
+  auto result =
+      cp::Declaration::Sequence(std::move(decls_)).AddToPlan(plan_.get());
+
+  if (!result.ok()) {
+    return folly::makeUnexpected(result.status().ToString());
+  }
+
+  return SamplesQuery::RunnableQuery{
+      std::move(plan_), arrow::schema(std::move(result_set_schema_)),
+      std::move(sink_gen), take_};
+}
+
+SamplesQuery::RunnableQuery::RunnableQuery(
+    std::shared_ptr<cp::ExecPlan> plan, std::shared_ptr<arrow::Schema> schema,
+    arrow::AsyncGenerator<std::optional<cp::ExecBatch>> sink_gen,
+    std::optional<int> take)
+    : plan_{std::move(plan)}, schema_{std::move(schema)},
+      sink_gen_{std::move(sink_gen)}, take_{take} {}
+
+namespace {
+arrow::Result<std::shared_ptr<arrow::Table>>
+genImpl(const std::shared_ptr<cp::ExecPlan> &plan,
+        std::shared_ptr<arrow::Schema> schema,
+        arrow::AsyncGenerator<std::optional<cp::ExecBatch>> sink_gen) {
+  auto sink_reader =
+      cp::MakeGeneratorReader(std::move(schema), std::move(sink_gen),
                               cp::default_exec_context()->memory_pool());
 
   ARROW_RETURN_NOT_OK(plan->StartProducing());
   std::shared_ptr<arrow::Table> result_set;
   ARROW_ASSIGN_OR_RAISE(result_set,
                         arrow::Table::FromRecordBatchReader(sink_reader.get()));
-  std::cout << "Results : " << result_set->Slice(0, 1)->ToString() << std::endl;
   plan->StopProducing();
 
   auto future = plan->finished();
-  return future.status();
+  ARROW_RETURN_NOT_OK(future.status());
+  return result_set;
 }
-
-arrow::Status do_taxi(const std::string &dataset_dir,
-                      const std::string &out_dir) {
-  ARROW_ASSIGN_OR_RAISE(auto file_sys,
-                        fs::FileSystemFromUriOrPath(dataset_dir));
-  ARROW_ASSIGN_OR_RAISE(auto dataset, get_dataset(dataset_dir));
-
-  auto *registry = cp::default_exec_factory_registry();
-  ds::internal::InitializeScanner(registry);
-  ds::internal::InitializeDatasetWriter(registry);
-  ARROW_ASSIGN_OR_RAISE(auto plan,
-                        cp::ExecPlan::Make(cp::default_exec_context()));
-
-  auto options = std::make_shared<ds::ScanOptions>();
-  options->projection = cp::project({}, {});
-  auto format = std::make_shared<arrow::dataset::ParquetFileFormat>();
-  arrow::AsyncGenerator<std::optional<cp::ExecBatch>> sink_gen;
-
-  auto status =
-      cp::Declaration::Sequence(
-          {
-              {"scan",
-               ds::ScanNodeOptions{
-                   dataset,
-                   options,
-               }},
-              {"filter", cp::FilterNodeOptions{cp::greater(
-
-                             cp::field_ref("total_amount"), cp::literal(30))}},
-              {"project",
-               cp::ProjectNodeOptions{{cp::field_ref("total_amount")}}},
-              {"sink", cp::SinkNodeOptions{&sink_gen}},
-          })
-          .AddToPlan(plan.get());
-
-  auto schema = arrow::schema({
-      arrow::field("total_amount", arrow::float64()),
-  });
-
-  return read_sink(std::move(plan), std::move(schema), std::move(sink_gen));
-}
-
 } // namespace
 
-void test_arrow() { XCHECK(do_taxi(FLAGS_dataset_dir, FLAGS_out_dir).ok()); }
+folly::Expected<std::shared_ptr<arrow::Table>, std::string>
+SamplesQuery::RunnableQuery::gen() && {
+  auto result_set = genImpl(plan_, std::move(schema_), std::move(sink_gen_));
+  if (!result_set.ok()) {
+    return folly::makeUnexpected(result_set.status().ToString());
+  }
+
+  if (!take_) {
+    return result_set.MoveValueUnsafe();
+  }
+
+  return result_set.MoveValueUnsafe()->Slice(0, take_.value());
+}
+
+SamplesQuery &SamplesQuery::take(int to_take) {
+  take_ = to_take;
+  return *this;
+}
+
+void test_arrow() {
+  auto table = BapidTable::fromFsDataset(FLAGS_dataset_dir, "taxi");
+  XCHECK(table.hasValue());
+  auto query = table.value()->newSamplesQueryX().filter().project().take(2);
+  auto result_set = std::move(query).finalize().value().gen().value();
+  std::cout << "Results : " << result_set->ToString() << std::endl;
+}
 } // namespace bapid
--- src/arrow.h
+++ src/arrow.h
@@ -1,5 +1,73 @@
+#include <arrow/api.h>
+#include <arrow/compute/exec/exec_plan.h>
+#include <arrow/dataset/file_parquet.h>
+#include <arrow/filesystem/filesystem.h>
+#include <folly/Expected.h>
+#include <memory>
+#include <optional>
 #include <string>
 
 namespace bapid {
+
+namespace fs = arrow::fs;
+namespace ds = arrow::dataset;
+namespace pq = parquet;
+namespace cp = arrow::compute;
+
+class SamplesQuery {
+public:
+  static folly::Expected<SamplesQuery, std::string>
+  fromDataset(std::shared_ptr<ds::Dataset> dataset);
+
+  SamplesQuery(cp::ExecFactoryRegistry *registry,
+               std::shared_ptr<cp::ExecPlan> plan,
+               std::shared_ptr<ds::Dataset> dataset);
+
+  class RunnableQuery {
+  public:
+    folly::Expected<std::shared_ptr<arrow::Table>, std::string> gen() &&;
+    RunnableQuery(std::shared_ptr<cp::ExecPlan> plan,
+                  std::shared_ptr<arrow::Schema> schema,
+                  arrow::AsyncGenerator<std::optional<cp::ExecBatch>> sink_gen,
+                  std::optional<int> take);
+
+  private:
+    std::shared_ptr<cp::ExecPlan> plan_;
+    std::shared_ptr<arrow::Schema> schema_;
+    arrow::AsyncGenerator<std::optional<cp::ExecBatch>> sink_gen_;
+    std::optional<int> take_;
+  };
+
+  // TODO(liuz): params
+  SamplesQuery &filter();
+  SamplesQuery &project();
+  SamplesQuery &take(int to_take);
+  folly::Expected<RunnableQuery, std::string> finalize() &&;
+
+private:
+  cp::ExecFactoryRegistry *registry_;
+  std::shared_ptr<cp::ExecPlan> plan_;
+  std::shared_ptr<ds::Dataset> dataset_;
+
+  std::vector<cp::Expression> filters_{};
+  std::vector<cp::Expression> projects_{};
+  std::vector<std::shared_ptr<arrow::Field>> result_set_schema_{};
+  std::vector<cp::Declaration> decls_{};
+  std::optional<int> take_;
+};
+
+class BapidTable {
+public:
+  static folly::Expected<std::unique_ptr<BapidTable>, std::string>
+  fromFsDataset(const std::string &dataset_dir, std::string name);
+
+  BapidTable(std::string name, std::shared_ptr<ds::Dataset> dataset);
+  SamplesQuery newSamplesQueryX();
+
+private:
+  std::string name_;
+  std::shared_ptr<ds::Dataset> dataset_;
+};
+
 void test_arrow();
-}
+} // namespace bapid
